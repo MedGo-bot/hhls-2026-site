ac9a06f69e1a5686e76eaa585a0849927f16fef9
Tracy D. Wilson <tracydwilson@besheanwe.com>
2026-02-05 12:43:12 -0800

    Create App1.tsx

---
+++ App1.tsx
@@ -0,0 +1,25 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import WhyAttend from './components/WhyAttend';
+import Vision from './components/Vision';
+import Speakers from './components/Speakers';
+import Pricing from './components/Pricing';
+import Sponsorship from './components/Sponsorship';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <WhyAttend />
+      <Vision />
+      <Speakers />
+      <Pricing />
+      <Sponsorship />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
